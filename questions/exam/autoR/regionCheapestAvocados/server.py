023388a1f20cf6db07196f74f8c3e4b13fc06ffd
Brian Kim <brian.kim.2011@gmail.com>
2024-02-20 13:37:30 -0800

    Fix dataframe code

--- questions/exam/autoR/regionCheapestAvocados/server.py
+++ questions/exam/autoR/regionCheapestAvocados/server.py
@@ -2,5 +2,5 @@ import prairielearn as pl
 import pandas as pd
 
 def generate(data):
-    df = pd.read_csv("avocado_prices.csv", header=None)
+    df = pd.read_csv(data["options"]["server_files_course_path"] + "/dataR/avocado_prices.csv", header=None)
     data["params"]["df"] = pl.to_json(df.head(15))
