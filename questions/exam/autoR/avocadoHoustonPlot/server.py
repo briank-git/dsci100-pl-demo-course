f60bac5bf916bf07a3a32ec3e46905e5ec8939d6
Brian Kim <brian.kim.2011@gmail.com>
2024-02-21 11:23:43 -0800

    Add data preview

---
+++ questions/exam/autoR/avocadoHoustonPlot/server.py
@@ -0,0 +1,6 @@
+import prairielearn as pl
+import pandas as pd
+
+def generate(data):
+    df = pd.read_csv(data["options"]["server_files_course_path"] + "/dataR/avocado_prices.csv", header=None)
+    data["params"]["df"] = pl.to_json(df.head(15))
