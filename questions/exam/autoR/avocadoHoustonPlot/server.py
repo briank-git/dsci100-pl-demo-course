112e32f7298c19fd28213b1d520e67acce92ca5b
Brian Kim <brian.kim.2011@gmail.com>
2024-02-21 11:25:01 -0800

    Reduce size of data preview

--- questions/exam/autoR/avocadoHoustonPlot/server.py
+++ questions/exam/autoR/avocadoHoustonPlot/server.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 def generate(data):
     df = pd.read_csv(data["options"]["server_files_course_path"] + "/dataR/avocado_prices.csv", header=None)
-    data["params"]["df"] = pl.to_json(df.head(15))
+    data["params"]["df"] = pl.to_json(df.head(5))
